b883ab1bedf80b52b91014f08c7f0317b4a1ed80
rohitn <rohitn@adobe.com>
2023-11-14 18:41:29 +0530

    Signed-off-by: rohitn <rohitn@adobe.com>

--- blocks/profile/profile.js
+++ blocks/profile/profile.js
@@ -2,7 +2,10 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     // console.log();
 
-    if (row.querySelector("p")) row.className = "profile-details";
-    row.className = "profile-images";
+    if (row.querySelector("p")) {
+      row.className = "profile-details";
+    } else {
+      row.className = "profile-images";
+    }
   });
 }
