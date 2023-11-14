90c22f8f2777768b98862177b672e7ce5eb6eb28
rohitn <rohitn@adobe.com>
2023-11-14 17:48:02 +0530

    Signed-off-by: rohitn <rohitn@adobe.com>

--- blocks/profile/profile.js
+++ blocks/profile/profile.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    row.classList.add(`mystyle ${row}`);
+    row.classList.add(`mystyle${row}`);
     console.log("under the profile card s");
   });
 }
