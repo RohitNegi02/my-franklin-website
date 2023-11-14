c5a24bdb5d32f9ed7e944a42f0b6990911b56014
rohitn <rohitn@adobe.com>
2023-11-14 18:13:20 +0530

    Signed-off-by: rohitn <rohitn@adobe.com>

--- blocks/profile/profile.js
+++ blocks/profile/profile.js
@@ -1,8 +1,8 @@
 export default function decorate(block) {
   [...block.children].forEach((div) => {
-    for (let i = 0; i <= div.childNodes.length - 1; i++) {
+    for (let i = 0; i <= div.children.length - 1; i++) {
       // check if the element is an IMG (image type).
-      console.log(div.childNodes[i].tagName);
+      console.log(div.children);
       if (div.childNodes[i].tagName == "picture") {
         div.className = "profile-image"; // show the ID of each image.
       } else {
