937cfbdf0a91a2ae3309e44b2147d283e9b96660
rohitn <rohitn@adobe.com>
2023-11-21 22:55:23 +0530

    Signed-off-by: rohitn <rohitn@adobe.com>

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -42,7 +42,7 @@ async function getWeather() {
       const markup = document.createElement("div");
       markup.classList.add("weather");
       console.log(result);
-      markup.innerHTML = `${result.current.temp_c}`;
+      markup.innerHTML = `${result.current.temp_c} °C`;
       headernav.insertAdjacentElement("beforeend", markup);
     })
     .catch((error) => console.log("error", error));
