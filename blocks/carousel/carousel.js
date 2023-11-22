e0f6a20df7335e7af63ed3d370988ca8fbc41f4f
rohitn <rohitn@adobe.com>
2023-11-22 17:54:28 +0530

    Signed-off-by: rohitn <rohitn@adobe.com>

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -25,7 +25,7 @@ export default function decorate(block) {
   rightButton.addEventListener("click", function () {
     currentSlide++;
     slides.forEach((s, i) => {
-      s.style.transform = `translateX(${100 * i - currentSlide}%)`;
+      s.style.transform = `translateX(${100 * (i - currentSlide)}%)`;
     });
   });
 }
