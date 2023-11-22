3cf525593650dc2fda294c409210bf20a069df8c
rohitn <rohitn@adobe.com>
2023-11-22 16:21:03 +0530

    Signed-off-by: rohitn <rohitn@adobe.com>

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -2,4 +2,8 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     row.className = "slide";
   });
+  const slides = document.querySelectorAll(".slide");
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX($(100*i)%)`;
+  });
 }
