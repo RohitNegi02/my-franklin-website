78eb8febfbbb76bdf763873dd8e1036f0f27aef7
rohitn <rohitn@adobe.com>
2023-11-22 17:14:40 +0530

    Signed-off-by: rohitn <rohitn@adobe.com>

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -9,6 +9,11 @@ export default function decorate(block) {
   leftButton.classList.add("slider__btn");
   leftButton.classList.add("slider__btn--left");
   leftButton.innerHTML = `&larr;`;
+  const rightButton = document.createElement("button");
+  slider.append(rightButton);
+  rightButton.classList.add("slider__btn");
+  rightButton.classList.add("slider__btn--right");
+  rightButton.innerHTML = `&rarr;`;
   slider.style.transform = "scale(0.5)";
   slider.style.overflow = "visible";
   const slides = document.querySelectorAll(".slide");
