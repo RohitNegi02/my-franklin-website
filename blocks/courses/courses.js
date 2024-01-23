06f60fa2da90e32ce42ab3b438abeaa2630586fa
rohitn <rohitn@adobe.com>
2024-01-23 18:15:37 +0530

    Signed-off-by: rohitn <rohitn@adobe.com>

--- blocks/courses/courses.js
+++ blocks/courses/courses.js
@@ -6,4 +6,21 @@ export default function decorate(block) {
   //   row.className = "profile-details";
   // });
   console.log("Hello Course");
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", "Bearer 435b1ec07f84682ee281eeba917db0bd");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://captivateprime.adobe.com/primeapi/v2/recommendations",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 }
