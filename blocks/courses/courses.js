e257b6aa0a082e60d35b1b5907e4011f58113cdc
rohitn <rohitn@adobe.com>
2024-01-23 17:52:52 +0530

    Signed-off-by: rohitn <rohitn@adobe.com>

---
+++ blocks/courses/courses.js
@@ -0,0 +1,9 @@
+export default function decorate(block) {
+  // [...block.children].forEach((row) => {
+  //   if (row.querySelector("picture")) {
+  //     row.className = "profile-images";
+  //   }
+  //   row.className = "profile-details";
+  // });
+  console.log("Hello Course");
+}
