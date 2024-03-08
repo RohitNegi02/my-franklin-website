8016b71f5497da961b53e72c44d71bc1f54a3533
RohitNegi02 <rohitnegi550@gmail.com>
2024-03-08 18:27:28 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -178,23 +178,25 @@ export default async function decorate(block) {
       return "";
     }
     async function handlePrimeLogIn() {
-      // const isLoggedIn = this.isLoggedIn();
-      const currentUrl = new URL(window.location.href);
-      const code = currentUrl.searchParams.get("code");
-
-      if (code) {
-        await fetchToken(code);
-      } else {
-        if (getCookie() == "") {
-          const markup = document.createElement("button");
+       const markup = document.createElement("button");
           markup.setAttribute("id", "myButton");
           markup.innerHTML = "LOG IN";
-          markup.addEventListener("click", () =>  document.location.href =
-        "https://learningmanager.adobe.com/oauth/o/authorize?account=121816&client_id=62f33554-103c-4fcb-b68c-d35c1d3da6a5&redirect_uri=http://localhost:4502/content/core-components-examples/library/adaptive-form/aemembedcontainer.html&state=prime_auth&scope=learner:read,learner:write&response_type=CODE&logoutAfterAuthorize=false";
-   );
-          nav.append(markup);
-        }
-      }
+          markup.addEventListener("click", () =>  getCpOauthUrl());
+      // const isLoggedIn = this.isLoggedIn();
+      // const currentUrl = new URL(window.location.href);
+      // const code = currentUrl.searchParams.get("code");
+
+      // if (code) {
+      //   await fetchToken(code);
+      // } else {
+      //   if (getCookie() == "") {
+      //     const markup = document.createElement("button");
+      //     markup.setAttribute("id", "myButton");
+      //     markup.innerHTML = "LOG IN";
+      //     markup.addEventListener("click", () =>  getCpOauthUrl() );
+      //     nav.append(markup);
+      //   }
+      // }
       //document.getElementById("myButton").onclick = getCpOauthUrl;
     }
     async function fetchToken(code) {
