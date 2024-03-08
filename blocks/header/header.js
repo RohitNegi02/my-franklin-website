ec706ebc55ae4abe079cb6322f92b1d14178357c
RohitNegi02 <rohitnegi550@gmail.com>
2024-03-08 18:25:48 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -189,7 +189,9 @@ export default async function decorate(block) {
           const markup = document.createElement("button");
           markup.setAttribute("id", "myButton");
           markup.innerHTML = "LOG IN";
-          markup.addEventListener("click", () => getCpOauthUrl());
+          markup.addEventListener("click", () =>  document.location.href =
+        "https://learningmanager.adobe.com/oauth/o/authorize?account=121816&client_id=62f33554-103c-4fcb-b68c-d35c1d3da6a5&redirect_uri=http://localhost:4502/content/core-components-examples/library/adaptive-form/aemembedcontainer.html&state=prime_auth&scope=learner:read,learner:write&response_type=CODE&logoutAfterAuthorize=false";
+   );
           nav.append(markup);
         }
       }
