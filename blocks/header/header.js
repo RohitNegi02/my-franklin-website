471faba2b51a102fe9bf8b83f1c70582a80240d0
RohitNegi02 <rohitnegi550@gmail.com>
2024-03-11 13:33:25 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -218,7 +218,7 @@ export default async function decorate(block) {
     }
     function getCpOauthUrl() {
       document.location.href =
-        "https://learningmanager.adobe.com/oauth/o/authorize?account=121816&client_id=62f33554-103c-4fcb-b68c-d35c1d3da6a5&redirect_uri=http://localhost:4504/content/wetrain/us/en.html&state=prime_auth&scope=learner:read,learner:write&response_type=CODE&logoutAfterAuthorize=false";
+        "https://learningmanager.adobe.com/oauth/o/authorize?account=121816&client_id=62f33554-103c-4fcb-b68c-d35c1d3da6a5&redirect_uri=http://localhost:4502/content/core-components-examples/library/adaptive-form/dateinput.html&state=prime_auth&scope=learner:read,learner:write&response_type=CODE&logoutAfterAuthorize=false";
     }
     // hamburger for mobile
     const hamburger = document.createElement("div");
