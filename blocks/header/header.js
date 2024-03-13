2e94f6f0dec1950f587cc94c5de3864fe772f3be
RohitNegi02 <rohitnegi550@gmail.com>
2024-03-13 17:20:57 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -270,7 +270,7 @@ fetch(`https://learningmanager.adobe.com/primeapi/v2/enrollments?loId=course:${c
       console.log("hello");
       document.location.href =
       //  `https://learningmanager.adobe.com//app/player?&user_id=20986942&account_id=121816&csrf_token=${access_token}&is_staged=false&preview=true&no_reporting=true&is_native=true&course_id=course:${course}&module_id=${instance}`;
-   `https://captivateprime.adobe.com/app/player?lo_id=${instance}&access_token=${access_token}`
+   `https://captivateprime.adobe.com/app/player?lo_id=${course}&access_token=${access_token}`
         }
     // hamburger for mobile
     const hamburger = document.createElement("div");
